734b78d342e344287adc4fbf8beb6144549b87f3
Thomas Topp <thomas.topp@nav.uni-stuttgart.de>
2020-06-26 14:10:49 +0200

    Conan.io offline file backup

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -3,6 +3,14 @@ macro(run_conan)
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
     file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake")
+
+    list(GET status 0 status_code)
+    list(GET status 1 status_string)
+
+    if(NOT status_code EQUAL 0)
+      message(STATUS "Could not download Conan Cmake file, using local backup")
+      file(COPY "cmake/Conan-v0.15/conan.cmake" DESTINATION "${CMAKE_BINARY_DIR}")
+    endif()
   endif()
 
   include(${CMAKE_BINARY_DIR}/conan.cmake)
