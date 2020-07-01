49cf00f03f1bd2cd6e394ffdf2ad3ae0274bf3f9
Thomas Topp <thomas.topp@nav.uni-stuttgart.de>
2020-07-01 10:21:15 +0200

    Fixed QMenuBar not showing under MacOS

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -411,14 +411,14 @@ int main(int argc, char* argv[])
     QWidget mainWidget;
 
     QMenuBar* menuBarLeft = new QMenuBar(&mainWidget);
-    menuBarLeft->setNativeMenuBar(true);
+    menuBarLeft->setNativeMenuBar(false);
     QAction* runAction = menuBarLeft->addAction("Run NavSoS");
     QAction* loadAction = menuBarLeft->addAction("Load");
     QAction* saveAction = menuBarLeft->addAction("Save As");
     QAction* clearAction = menuBarLeft->addAction("Clear");
     QAction* clearExit = menuBarLeft->addAction("Exit");
 
-    QMenuBar* menuBarRight = new QMenuBar();
+    QMenuBar* menuBarRight = new QMenuBar(&mainWidget);
     menuBarRight->setNativeMenuBar(false);
     rtpAction = menuBarRight->addAction("All Nodes");
 
