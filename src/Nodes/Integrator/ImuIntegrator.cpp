74cb04ae1baed1626962571cccab602f26ad481a
Thomas Topp <thomas.topp@nav.uni-stuttgart.de>
2020-08-22 16:53:49 +0200

    No Unicode in variable names

--- src/Nodes/Integrator/ImuIntegrator.cpp
+++ src/Nodes/Integrator/ImuIntegrator.cpp
@@ -23,54 +23,53 @@ void NAV::ImuIntegrator::integrateObservation(std::shared_ptr<NAV::ImuObs>& obs)
         prevStates.push_back(currentStateData);
     }
 
-    /// Time at 2 previous epochs
-    const auto& tₖ₋₂ = prevObs.at(1)->insTime.value();
-    /// Time at previous epoch
-    const auto& tₖ₋₁ = prevObs.at(0)->insTime.value();
-    /// Current Time
-    const auto& tₖ = obs->insTime.value();
+    /// tₖ₋₂ Time at 2 previous epochs
+    const auto& t__k2 = prevObs.at(1)->insTime.value();
+    /// tₖ₋₁ Time at previous epoch
+    const auto& t__k1 = prevObs.at(0)->insTime.value();
+    /// tₖ Current Time
+    const auto& t__k = obs->insTime.value();
 
-    /// Rotation rate in [rad/s],
+    /// ω_ip_p (tₖ₋₁) Rotation rate in [rad/s],
     /// of the inertial to platform system, in platform coordinates, at the time tₖ₋₁
-    const auto& ω_ip_p__tₖ₋₁ = prevObs.at(0)->gyroUncompXYZ.value();
-    /// Rotation rate in [rad/s],
+    const auto& omega_ip_p__k1 = prevObs.at(0)->gyroUncompXYZ.value();
+    /// ω_ip_p (tₖ) Rotation rate in [rad/s],
     /// of the inertial to platform system, in platform coordinates, at the time tₖ
-    const auto& ω_ip_p__tₖ = obs->gyroUncompXYZ.value();
+    const auto& omega_ip_p__k = obs->gyroUncompXYZ.value();
 
-    // Delta time in [seconds] (tₖ₋₁ - tₖ₋₂)
-    auto Δtₖ₋₁ = (tₖ₋₁ - tₖ₋₂).count();
-    // Delta time in [seconds] (tₖ - tₖ₋₁)
-    auto Δtₖ = (tₖ - tₖ₋₁).count();
+    // Δtₖ₋₁ = (tₖ₋₁ - tₖ₋₂) Time difference in [seconds]
+    auto Dt__k1 = (t__k1 - t__k2).count();
+    // Δtₖ = (tₖ - tₖ₋₁) Time difference in [seconds]
+    auto Dt__k = (t__k - t__k1).count();
 
-    /// The delta rotation angles in [radian],
+    /// Δα_ip_p (tₖ₋₁) The delta rotation angles in [radian],
     /// of the inertial to platform system, in platform coordinates, at the time tₖ₋₁
-    Eigen::Vector3d Δα_ip_p__tₖ₋₁ = Δtₖ₋₁ * ω_ip_p__tₖ₋₁;
-    /// The delta rotation angles in [radian],
+    Eigen::Vector3d Dalpha_ip_p__k1 = Dt__k1 * omega_ip_p__k1;
+    /// Δα_ip_p (tₖ) The delta rotation angles in [radian],
     /// of the inertial to platform system, in platform coordinates, at the time tₖ
-    Eigen::Vector3d Δα_ip_p__tₖ = Δtₖ * ω_ip_p__tₖ;
+    Eigen::Vector3d Dalpha_ip_p__k = Dt__k * omega_ip_p__k;
 
-    /// Direction Cosine Matrix at the time tₖ₋₂
-    const auto& DCM_e2p__tₖ₋₂ = prevStates.at(1)->DCM_e2p();
-    /// Direction Cosine Matrix at the time tₖ₋₁
-    const auto& DCM_e2p__tₖ₋₁ = prevStates.at(0)->DCM_e2p();
+    /// C_ep (tₖ₋₂) Direction Cosine Matrix at the time tₖ₋₂
+    const auto& DCM_e2p__k2 = prevStates.at(1)->DCM_e2p();
+    /// C_ep (tₖ₋₁) Direction Cosine Matrix at the time tₖ₋₁
+    const auto& DCM_e2p__k1 = prevStates.at(0)->DCM_e2p();
 
-    /// Rotation rate in [rad/s],
+    /// ω_ie_e (tₖ) Rotation rate in [rad/s],
     /// of the inertial to earth system, in platform coordinates, at the time tₖ
-    Eigen::Vector3d ω_ie_e;
+    Eigen::Vector3d omega_ie_e = Eigen::Vector3d::Ones();
 
-    /// The delta rotation angles in [radian],
+    /// Δβ⁠_ep_p (tₖ₋₁) The delta rotation angles in [radian],
     /// of the earth to platform system, in platform coordinates, at the time tₖ₋₁
-    Eigen::Vector3d Δ⁠β⁠_ep_p__tₖ₋₁ = Δα_ip_p__tₖ₋₁ - DCM_e2p__tₖ₋₂ * ω_ie_e * Δtₖ₋₁;
-    /// The delta rotation angles in [radian],
+    Eigen::Vector3d Dbeta_ep_p__k1 = Dalpha_ip_p__k1 - DCM_e2p__k2 * omega_ie_e * Dt__k1;
+    /// Δβ⁠_ep_p (tₖ) The delta rotation angles in [radian],
     /// of the earth to platform system, in platform coordinates, at the time tₖ
-    Eigen::Vector3d Δβ⁠_ep_p__tₖ = Δα_ip_p__tₖ - DCM_e2p__tₖ₋₁ * ω_ie_e * Δtₖ;
+    Eigen::Vector3d Dbeta_ep_p__k = Dalpha_ip_p__k - DCM_e2p__k1 * omega_ie_e * Dt__k;
 
-    LOG_INFO("Δ⁠β⁠_ep_p__tₖ₋₁: {}", Δ⁠β⁠_ep_p__tₖ₋₁);
-    LOG_INFO("Δβ⁠_ep_p__tₖ: {}", Δβ⁠_ep_p__tₖ);
+    LOG_INFO("Δ⁠β⁠_ep_p__tₖ₋₁: {}", Dbeta_ep_p__k1);
+    LOG_INFO("Δβ⁠_ep_p__tₖ: {}", Dbeta_ep_p__k);
     // auto dt = 2 * delta_t;
 
     // omega_ep^p = omega_ip^p - C_e^p * omega_ie^e
-    // omega_ip^p = gyroUncompXYZ
 
     // Rotate Data
     if (prevObs.size() == 2)
