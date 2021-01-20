93719ae00af371cea01a3b95b38d9b78f8e2ca09
Thomas Topp <topp@ins.uni-stuttgart.de>
2021-01-20 10:16:57 +0100

    clang-tidy chrono <=> fix

--- src/Nodes/util/Demo.cpp
+++ src/Nodes/util/Demo.cpp
@@ -386,7 +386,10 @@ bool NAV::Demo::initialize()
     LOG_TRACE("{}: called", nameId());
 
     // To Show the Initialization in the GUI
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    std::this_thread::sleep_until(std::chrono::high_resolution_clock::now() + std::chrono::milliseconds(3000));
+
+    // Currently crashes clang-tidy (Stack dump: #0 Calling std::chrono::operator<=>), so use sleep_until
+    // std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 
     receivedDataFromSensorCnt = 0;
     receivedDataFromFileReaderCnt = 0;
