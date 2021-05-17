eea9929d299daf08f29ac14c5c6070b709932c35
Marcel Maier <marcel.maier@ins.uni-stuttgart.de>
2021-05-17 12:10:45 +0200

    Boost Network Stream added to SkydelImuStream Node

--- src/Nodes/DataProvider/IMU/NetworkStream/SkydelImuStream.cpp
+++ src/Nodes/DataProvider/IMU/NetworkStream/SkydelImuStream.cpp
@@ -11,14 +11,35 @@ namespace nm = NAV::NodeManager;
 
 #include "util/Time/TimeBase.hpp"
 
-NAV::SkydelImuStream::SkydelImuStream()
+// Includes for IMU Stream
+#include <cstdlib>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+
+using boost::asio::ip::udp;
+//using namespace boost::algorithm;
+//using boost::algorithm;
+
+NAV::SkydelImuStream::SkydelImuStream() : port(4444), sender_endpoint_(udp::v4(), port), data_({}), socket_(ioservice, sender_endpoint_)
+
 {
     name = typeStatic();
 
-    LOG_TRACE("{}: called", name);
+    // LOG_TRACE("{}: called", name);
 
     color = ImColor(255, 128, 128);
     hasConfig = true;
+    // breakStream = false;
+
+    //port = 4444;
+    //sender_endpoint_ = boost::asio::ip::udp::endpoint(udp::v4(), port);
+    // socket_ = boost::asio::ip::udp::socket(ioservice, sender_endpoint_);
 
     nm::CreateOutputPin(this, "ImuObs", Pin::Type::Flow, NAV::ImuObs::type());
 }
@@ -45,11 +66,23 @@ std::string NAV::SkydelImuStream::category()
 
 void NAV::SkydelImuStream::guiConfig()
 {
+    /*  // Checkbox Beispiel von TT
+    static bool chk1 = false;
+    if (ImGui::Checkbox("Test##1", &chk1))
+    {
+        LOG_DEBUG("Test Button 1");
+    }
+    static bool chk2 = false;
+    if (ImGui::Checkbox("Test##2", &chk2))
+    {
+        LOG_DEBUG("Test Button 2");
+    }
+    */
 }
 
 [[nodiscard]] json NAV::SkydelImuStream::save() const
 {
-    LOG_TRACE("{}: called", nameId());
+    //LOG_TRACE("{}: called", nameId());
 
     json j;
 
@@ -73,9 +106,82 @@ bool NAV::SkydelImuStream::resetNode()
     return true;
 }
 
+void NAV::SkydelImuStream::do_receive()
+{
+    socket_.async_receive_from(
+        boost::asio::buffer(data_, max_length), sender_endpoint_,
+        [this](boost::system::error_code ed, std::size_t sdfe) {
+            // std::cout << ed << sdfe;
+            if ((!ed) & (sdfe > 0)) {}
+            std::vector<std::string> v;
+            boost::algorithm::split(v, data_, boost::is_any_of(","));
+            //std::cout << std::stod(v.at(0)) *10.0 << '\n';
+            //std::cout << data_;
+            // LOG_TRACE("{}: reading 4444", data_);
+
+            // LOG_TRACE("mm: Test");
+            //if (InsTime currentTime = util::time::GetCurrentTime();
+            //   !currentTime.empty())
+            //{
+            //    obs->in sTime = currentTime;
+            //}
+
+            if (v.size() == 7)
+            {
+                auto obs = std::make_shared<ImuObs>(this->imuPos);
+                obs->timeSinceStartup = std::stod(v.at(0)) * 1e6;
+
+                // if (startTime.empty())
+                // {
+                //     startTime = util::time::GetCurrentTime();
+                //     timeSinceStartupStart = obs->timeSinceStartup.value();
+                // }
+
+                // obs->insTime = startTime + std::chrono::milliseconds((obs->timeSinceStartup.value() - timeSinceStartupStart));
+
+                obs->accelUncompXYZ.emplace(std::stod(v.at(1)), std::stod(v.at(2)), std::stod(v.at(3)));
+                obs->gyroUncompXYZ.emplace(std::stod(v.at(4)), std::stod(v.at(5)), std::stod(v.at(6)));
+                obs->magUncompXYZ.emplace(0.0, 0.0, 0.0);
+
+                // Calls all the callbacks
+                if (InsTime currentTime = util::time::GetCurrentTime();
+                    !currentTime.empty())
+                {
+                    obs->insTime = currentTime;
+                }
+                this->invokeCallbacks(OutputPortIndex_ImuObs, obs);
+            }
+
+            // if (!breakStream)
+            // {
+            do_receive();
+            // }
+            // else
+            // {
+            //     TestThread.join();
+            // }
+        });
+}
+
 bool NAV::SkydelImuStream::initialize()
 {
-    LOG_TRACE("{}: called", nameId());
+    //    sensor = std::make_unique<>();
+
+    //    int ReceiverThread()
+    //  {
+    // breakStream = false;
+
+    do_receive();
+
+    TestThread = std::thread([=, this]() { ioservice.run(); });
+    //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    //io_context.run();
+
+    //return 0;
+    //};
+
+    LOG_TRACE("{}: initialized", name);
 
     return true;
 }
@@ -83,21 +189,34 @@ bool NAV::SkydelImuStream::initialize()
 void NAV::SkydelImuStream::deinitialize()
 {
     LOG_TRACE("{}: called", nameId());
+
+    // breakStream = true;
+
+    // LOG_TRACE("{}: de-initialized", nameId());
+
+    // if (!isInitialized())
+    // {
+    //     return;
+    // }
+
+    //TestThread.join();
+
+    // startTime = InsTime{};
 }
 
-// void NAV::SkydelImuStream::readImuThread()
-// void NAV::SkydelImuStream::readImuThread(void* userData)
-// {
-//     auto* navio = static_cast<SkydelImuStream*>(userData);
-// auto obs = std::make_shared<ImuObs>(navio->imuPos);
-
-//     LOG_DATA("DATA({}): {}, {}°C, a=({}, {}, {})", navio->name, obs->timeSinceStartup.value(), obs->temperature.value(),
-//              navio->ax, navio->ay, navio->az);
-
-//     if (InsTime currentTime = util::time::GetCurrentTime();
-//         !currentTime.empty())
-//     {
-//         obs->insTime = currentTime;
-//     }
-// navio->invokeCallbacks(OutputPortIndex_ImuObs, obs);
-// }
+void NAV::SkydelImuStream::readImuThread(void* userData)
+{
+    //LOG_TRACE("mm: Test");
+    //std::cout << userData << std::endl;
+    //return true;
+    //std::cout << "mmTest" << std::endl;
+    auto* skydel = static_cast<SkydelImuStream*>(userData);
+    auto obs = std::make_shared<ImuObs>(skydel->imuPos);
+    LOG_TRACE("mm: Test");
+    //if (InsTime currentTime = util::time::GetCurrentTime();
+    //   !currentTime.empty())
+    //{
+    //    obs->insTime = currentTime;
+    //}
+    skydel->invokeCallbacks(OutputPortIndex_ImuObs, obs);
+}
--- src/Nodes/DataProvider/IMU/NetworkStream/SkydelImuStream.hpp
+++ src/Nodes/DataProvider/IMU/NetworkStream/SkydelImuStream.hpp
@@ -7,6 +7,18 @@
 
 #include "Nodes/DataProvider/IMU/Imu.hpp"
 
+#include <cstdlib>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+
+#include "util/InsTime.hpp"
+
 namespace NAV
 {
 /// SkydelImuStream Sensor Class
@@ -49,6 +61,12 @@ class SkydelImuStream : public Imu
     /// @brief Resets the node. It is guaranteed that the node is initialized when this is called.
     bool resetNode() override;
 
+    void do_receive();
+
+    // Asynchronous receive fct
+    //void async_receive_from();
+    boost::asio::io_context ioservice;
+
   private:
     constexpr static size_t OutputPortIndex_ImuObs = 1; ///< @brief Flow (ImuObs)
 
@@ -58,9 +76,58 @@ class SkydelImuStream : public Imu
     /// @brief Deinitialize the node
     void deinitialize() override;
 
+    // Thread for receiver fct
+    int ReceiverThread();
+
+    std::thread TestThread;
+    enum
+    {
+        max_length = 1024
+    };
+
+    uint16_t port;
+    boost::asio::ip::udp::endpoint sender_endpoint_;
+    std::array<char, max_length> data_;
+    boost::asio::ip::udp::socket socket_;
+    // bool breakStream;
+
+    // InsTime startTime;
+    // uint64_t timeSinceStartupStart = 0;
+
+    /*
+    /// The Imu type
+    ImuType imuType = ImuType::MPU;
+
+    /// OutputFrequency to calculate rateDivisor field.
+    int outputFrequency = 100;
+
+    /// Timer object to handle async data requests
+    CallbackTimer timer;
+    /// Start Time to calculate the TimeSinceStartup
+    std::chrono::time_point<std::chrono::steady_clock> startTime;
+
+    /// Accelerometer X data, which are read into by the sensor
+    float ax{};
+    /// Accelerometer Y data, which are read into by the sensor
+    float ay{};
+    /// Accelerometer Z data, which are read into by the sensor
+    float az{};
+    /// Gyroscope X data, which are read into by the sensor
+    float gx{};
+    /// Gyroscope Y data, which are read into by the sensor
+    float gy{};
+    /// Gyroscope Z data, which are read into by the sensor
+    float gz{};
+    /// Magnetometer X data, which are read into by the sensor
+    float mx{};
+    /// Magnetometer Y data, which are read into by the sensor
+    float my{};
+    /// Magnetometer Z data, which are read into by the sensor
+    float mz{};
+*/
     /// @brief Function which performs the async data reading
     /// @param[in, out] userData Pointer to the SkydelImuStream object
-    // static void readImuThread(void* userData);
+    static void readImuThread(void* userData);
 };
 
 } // namespace NAV
