246c9ce98e653db2ab46101f49cbd2203ec19b4a
Thomas Topp <thomas@topp.cc>
2021-10-07 09:07:33 +0200

    Fix tests

--- test/src/NodeData/State/StateDataTests.cpp
+++ test/src/NodeData/State/StateDataTests.cpp
@@ -10,22 +10,22 @@ TEST_CASE("[PosVelAtt] Reference Functions", "[PosVelAtt]")
 
     auto quat_nb_coeff = Eigen::Vector4d(1, 2, 3, 4);
 
-    state.quaternion_nb().coeffs() = quat_nb_coeff;
+    state.setAttitude_nb(Eigen::Quaterniond(quat_nb_coeff));
 
     CHECK(state.quaternion_nb().coeffs() == quat_nb_coeff);
 
     quat_nb_coeff = Eigen::Vector4d(5, 6, 7, 8);
     auto quat_nb = Eigen::Quaterniond(quat_nb_coeff);
 
-    state.quaternion_nb() = quat_nb;
+    state.setAttitude_nb(quat_nb);
 
     CHECK(state.quaternion_nb().coeffs() == quat_nb.coeffs());
 
     /* -------------------------------------------------------------------------------------------------------- */
 
-    Eigen::Vector3d latLonAlt = Eigen::Vector3d(5, 6, 7);
+    Eigen::Vector3d latLonAlt = Eigen::Vector3d(trafo::deg2rad(5), trafo::deg2rad(6), 7);
     auto pos_e = trafo::lla2ecef_WGS84(latLonAlt);
-    state.position_ecef() = pos_e;
+    state.setPosition_e(pos_e, latLonAlt);
 
     CHECK(state.position_ecef() == pos_e);
     CHECK(state.latLonAlt() == trafo::ecef2lla_WGS84(pos_e));
@@ -34,18 +34,18 @@ TEST_CASE("[PosVelAtt] Reference Functions", "[PosVelAtt]")
     CHECK(state.altitude() == trafo::ecef2lla_WGS84(pos_e)(2));
 
     // Supposed to do nothing, as we overwrite temporary variable
-    state.latLonAlt() = latLonAlt;
+    state.setPosition_lla(latLonAlt, latLonAlt);
 
-    CHECK(state.latLonAlt() == trafo::ecef2lla_WGS84(pos_e));
-    CHECK(state.latitude() == trafo::ecef2lla_WGS84(pos_e)(0));
-    CHECK(state.longitude() == trafo::ecef2lla_WGS84(pos_e)(1));
-    CHECK(state.altitude() == trafo::ecef2lla_WGS84(pos_e)(2));
+    CHECK(state.latLonAlt() == latLonAlt);
+    CHECK(state.latitude() == latLonAlt(0));
+    CHECK(state.longitude() == latLonAlt(1));
+    CHECK(state.altitude() == latLonAlt(2));
 
     /* -------------------------------------------------------------------------------------------------------- */
 
     auto vel_n = Eigen::Vector3d(5, 6, 7);
 
-    state.velocity_n() = vel_n;
+    state.setVelocity_n(vel_n);
 
     CHECK(state.velocity_n() == vel_n);
 }
