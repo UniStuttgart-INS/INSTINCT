47c5a4c35accf9a93ddbb72800ebfbc40c39b53b
Thomas Topp <thomas@topp.cc>
2021-10-18 16:54:13 +0200

    Fixed compiler errors

--- src/util/StringUtil.hpp
+++ src/util/StringUtil.hpp
@@ -74,7 +74,7 @@ static inline bool replace(std::string& str, const std::string& from, const std:
 {
     if (from.empty())
     {
-        return;
+        return false;
     }
     size_t start_pos = str.find(from);
     if (start_pos == std::string::npos)
