83c5583fa0eec04ecbfc58731a0e9492044b219a
Thomas Topp <thomas@topp.cc>
2021-11-24 22:45:59 +0100

    TimeEdit with Limited ImGui Elements

--- src/internal/gui/widgets/TimeEdit.cpp
+++ src/internal/gui/widgets/TimeEdit.cpp
@@ -3,6 +3,8 @@
 #include <imgui.h>
 #include <fmt/core.h>
 
+#include "internal/gui/widgets/imgui_ex.hpp"
+
 bool NAV::gui::widgets::TimeEdit(const char* str_id, InsTime& insTime, TimeEditFormat& timeEditFormat, float itemWidth)
 {
     bool changes = false;
@@ -28,7 +30,7 @@ bool NAV::gui::widgets::TimeEdit(const char* str_id, InsTime& insTime, TimeEditF
         int day = ymdhms.day;
         int hour = ymdhms.hour;
         int min = ymdhms.min;
-        double sec = static_cast<double>(ymdhms.sec);
+        auto sec = static_cast<double>(ymdhms.sec);
 
         ImGui::SetNextItemWidth(itemWidth);
         if (ImGui::InputInt(fmt::format("Year##{}", str_id).c_str(), &year, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
@@ -36,27 +38,27 @@ bool NAV::gui::widgets::TimeEdit(const char* str_id, InsTime& insTime, TimeEditF
             changes = true;
         }
         ImGui::SetNextItemWidth(itemWidth);
-        if (ImGui::InputInt(fmt::format("Month##{}", str_id).c_str(), &month, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
+        if (ImGui::InputIntL(fmt::format("Month##{}", str_id).c_str(), &month, 1, InsTimeUtil::MONTHS_PER_YEAR, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
         {
             changes = true;
         }
         ImGui::SetNextItemWidth(itemWidth);
-        if (ImGui::InputInt(fmt::format("Day##{}", str_id).c_str(), &day, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
+        if (ImGui::InputIntL(fmt::format("Day##{}", str_id).c_str(), &day, 1, InsTimeUtil::daysInMonth(month, year), 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
         {
             changes = true;
         }
         ImGui::SetNextItemWidth(itemWidth);
-        if (ImGui::InputInt(fmt::format("Hour##{}", str_id).c_str(), &hour, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
+        if (ImGui::InputIntL(fmt::format("Hour##{}", str_id).c_str(), &hour, 0, InsTimeUtil::HOURS_PER_DAY - 1, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
         {
             changes = true;
         }
         ImGui::SetNextItemWidth(itemWidth);
-        if (ImGui::InputInt(fmt::format("Min##{}", str_id).c_str(), &min, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
+        if (ImGui::InputIntL(fmt::format("Min##{}", str_id).c_str(), &min, 0, InsTimeUtil::MINUTES_PER_HOUR - 1, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
         {
             changes = true;
         }
         ImGui::SetNextItemWidth(itemWidth);
-        if (ImGui::InputDouble(fmt::format("Sec##{}", str_id).c_str(), &sec, 0, 0, "%.6f", ImGuiInputTextFlags_EnterReturnsTrue))
+        if (ImGui::InputDoubleL(fmt::format("Sec##{}", str_id).c_str(), &sec, 0, InsTimeUtil::SECONDS_PER_MINUTE - 1, 0, 0, "%.6f", ImGuiInputTextFlags_EnterReturnsTrue))
         {
             changes = true;
         }
@@ -73,37 +75,21 @@ bool NAV::gui::widgets::TimeEdit(const char* str_id, InsTime& insTime, TimeEditF
 
         int cycle = gpsWeekTow.gpsCycle;
         int week = gpsWeekTow.gpsWeek;
-        double tow = static_cast<double>(gpsWeekTow.tow);
+        auto tow = static_cast<double>(gpsWeekTow.tow);
 
         ImGui::SetNextItemWidth(itemWidth);
-        if (ImGui::InputInt(fmt::format("Cycle##{}", str_id).c_str(), &cycle, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
+        if (ImGui::InputIntL(fmt::format("Cycle##{}", str_id).c_str(), &cycle, 0, std::numeric_limits<int>::max(), 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
         {
-            if (cycle < 0)
-            {
-                cycle = 0;
-            }
             changes = true;
         }
         ImGui::SetNextItemWidth(itemWidth);
-        if (ImGui::InputInt(fmt::format("Week##{}", str_id).c_str(), &week, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
+        if (ImGui::InputIntL(fmt::format("Week##{}", str_id).c_str(), &week, 0, InsTimeUtil::WEEKS_PER_GPS_CYCLE - 1, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue))
         {
-            if (week < 0)
-            {
-                week = 0;
-            }
-            if (week >= InsTimeUtil::WEEKS_PER_GPS_CYCLE)
-            {
-                cycle = 0;
-            }
             changes = true;
         }
         ImGui::SetNextItemWidth(itemWidth);
-        if (ImGui::InputDouble(fmt::format("ToW [s]##{}", str_id).c_str(), &tow, 0, 0, "%.6f", ImGuiInputTextFlags_EnterReturnsTrue))
+        if (ImGui::InputDoubleL(fmt::format("ToW [s]##{}", str_id).c_str(), &tow, 0, std::numeric_limits<double>::max(), 0, 0, "%.6f", ImGuiInputTextFlags_EnterReturnsTrue))
         {
-            if (tow < 0)
-            {
-                tow = 0;
-            }
             changes = true;
         }
 
