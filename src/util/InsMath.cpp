7b9a56cb410d2d15662f9d54036362c754a543dd
Thomas Topp <thomas@topp.cc>
2021-11-25 15:09:37 +0100

    Fix: calcGreatCircleDistance uses  radius not a

--- src/util/InsMath.cpp
+++ src/util/InsMath.cpp
@@ -6,7 +6,7 @@
 
 double NAV::calcGreatCircleDistance(double lat1, double lon1, double lat2, double lon2)
 {
-    double R = InsConst::WGS84_a;
+    double R = geocentricRadius(lat1, earthRadius_E(lat1));
     double dLat = lat2 - lat1;
     double dLon = lon2 - lon1;
     double a = std::pow(std::sin(dLat / 2.0), 2) + std::cos(lat1) * std::cos(lat2) * std::pow(std::sin(dLon / 2.0), 2);
