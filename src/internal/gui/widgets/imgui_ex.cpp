35a2d80b6f16fac2b03104d4752750d9de3261e6
Thomas Topp <thomas@topp.cc>
2021-12-23 17:28:04 +0100

    Added ImGui DragLong/DragULong+SliderLong/SliderULong

--- src/internal/gui/widgets/imgui_ex.cpp
+++ src/internal/gui/widgets/imgui_ex.cpp
@@ -2,70 +2,161 @@
 
 #include <algorithm>
 
+namespace ImGui
+{
+
 // ###########################################################################################################
 
-bool ImGui::DragDouble(const char* label, double* v, float v_speed, double v_min, double v_max, const char* format, ImGuiSliderFlags flags)
+bool DragDouble(const char* label, double* v, float v_speed, double v_min, double v_max, const char* format, ImGuiSliderFlags flags)
 {
     return DragScalar(label, ImGuiDataType_Double, v, v_speed, &v_min, &v_max, format, flags);
 }
 
-bool ImGui::DragDouble2(const char* label, double v[2], float v_speed, double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool DragDouble2(const char* label, double v[2], float v_speed, double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     return DragScalarN(label, ImGuiDataType_Double, v, 2, v_speed, &v_min, &v_max, format, flags);
 }
 
-bool ImGui::DragDouble3(const char* label, double v[3], float v_speed, double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool DragDouble3(const char* label, double v[3], float v_speed, double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     return DragScalarN(label, ImGuiDataType_Double, v, 3, v_speed, &v_min, &v_max, format, flags);
 }
 
-bool ImGui::DragDouble4(const char* label, double v[4], float v_speed, double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool DragDouble4(const char* label, double v[4], float v_speed, double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     return DragScalarN(label, ImGuiDataType_Double, v, 4, v_speed, &v_min, &v_max, format, flags);
 }
 
 // ###########################################################################################################
 
-bool ImGui::SliderDouble(const char* label, double* v, double v_min, double v_max, const char* format, ImGuiSliderFlags flags)
+bool DragLong(const char* label, int64_t* v, float v_speed, int64_t v_min, int64_t v_max, const char* format, ImGuiSliderFlags flags)
+{
+    return DragScalar(label, ImGuiDataType_S64, v, v_speed, &v_min, &v_max, format, flags);
+}
+
+bool DragLong2(const char* label, int64_t v[2], float v_speed, int64_t v_min, int64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return DragScalarN(label, ImGuiDataType_S64, v, 2, v_speed, &v_min, &v_max, format, flags);
+}
+
+bool DragLong3(const char* label, int64_t v[3], float v_speed, int64_t v_min, int64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return DragScalarN(label, ImGuiDataType_S64, v, 3, v_speed, &v_min, &v_max, format, flags);
+}
+
+bool DragLong4(const char* label, int64_t v[4], float v_speed, int64_t v_min, int64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return DragScalarN(label, ImGuiDataType_S64, v, 4, v_speed, &v_min, &v_max, format, flags);
+}
+
+// ###########################################################################################################
+
+bool DragULong(const char* label, uint64_t* v, float v_speed, uint64_t v_min, uint64_t v_max, const char* format, ImGuiSliderFlags flags)
+{
+    return DragScalar(label, ImGuiDataType_U64, v, v_speed, &v_min, &v_max, format, flags);
+}
+
+bool DragULong2(const char* label, uint64_t v[2], float v_speed, uint64_t v_min, uint64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return DragScalarN(label, ImGuiDataType_U64, v, 2, v_speed, &v_min, &v_max, format, flags);
+}
+
+bool DragULong3(const char* label, uint64_t v[3], float v_speed, uint64_t v_min, uint64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return DragScalarN(label, ImGuiDataType_U64, v, 3, v_speed, &v_min, &v_max, format, flags);
+}
+
+bool DragULong4(const char* label, uint64_t v[4], float v_speed, uint64_t v_min, uint64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return DragScalarN(label, ImGuiDataType_U64, v, 4, v_speed, &v_min, &v_max, format, flags);
+}
+
+// ###########################################################################################################
+
+bool SliderDouble(const char* label, double* v, double v_min, double v_max, const char* format, ImGuiSliderFlags flags)
 {
     return SliderScalar(label, ImGuiDataType_Double, v, &v_min, &v_max, format, flags);
 }
 
-bool ImGui::SliderDouble2(const char* label, double v[2], double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool SliderDouble2(const char* label, double v[2], double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     return SliderScalarN(label, ImGuiDataType_Double, v, 2, &v_min, &v_max, format, flags);
 }
 
-bool ImGui::SliderDouble3(const char* label, double v[3], double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool SliderDouble3(const char* label, double v[3], double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     return SliderScalarN(label, ImGuiDataType_Double, v, 3, &v_min, &v_max, format, flags);
 }
 
-bool ImGui::SliderDouble4(const char* label, double v[4], double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool SliderDouble4(const char* label, double v[4], double v_min, double v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     return SliderScalarN(label, ImGuiDataType_Double, v, 4, &v_min, &v_max, format, flags);
 }
 
 // ###########################################################################################################
 
-bool ImGui::InputDouble2(const char* label, double v[2], const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool SliderLong(const char* label, int64_t* v, int64_t v_min, int64_t v_max, const char* format, ImGuiSliderFlags flags)
+{
+    return SliderScalar(label, ImGuiDataType_S64, v, &v_min, &v_max, format, flags);
+}
+
+bool SliderLong2(const char* label, int64_t v[2], int64_t v_min, int64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return SliderScalarN(label, ImGuiDataType_S64, v, 2, &v_min, &v_max, format, flags);
+}
+
+bool SliderLong3(const char* label, int64_t v[3], int64_t v_min, int64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return SliderScalarN(label, ImGuiDataType_S64, v, 3, &v_min, &v_max, format, flags);
+}
+
+bool SliderLong4(const char* label, int64_t v[4], int64_t v_min, int64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return SliderScalarN(label, ImGuiDataType_S64, v, 4, &v_min, &v_max, format, flags);
+}
+
+// ###########################################################################################################
+
+bool SliderULong(const char* label, uint64_t* v, uint64_t v_min, uint64_t v_max, const char* format, ImGuiSliderFlags flags)
+{
+    return SliderScalar(label, ImGuiDataType_U64, v, &v_min, &v_max, format, flags);
+}
+
+bool SliderULong2(const char* label, uint64_t v[2], uint64_t v_min, uint64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return SliderScalarN(label, ImGuiDataType_U64, v, 2, &v_min, &v_max, format, flags);
+}
+
+bool SliderULong3(const char* label, uint64_t v[3], uint64_t v_min, uint64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return SliderScalarN(label, ImGuiDataType_U64, v, 3, &v_min, &v_max, format, flags);
+}
+
+bool SliderULong4(const char* label, uint64_t v[4], uint64_t v_min, uint64_t v_max, const char* format, ImGuiSliderFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+{
+    return SliderScalarN(label, ImGuiDataType_U64, v, 4, &v_min, &v_max, format, flags);
+}
+
+// ###########################################################################################################
+
+bool InputDouble2(const char* label, double v[2], const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     return InputScalarN(label, ImGuiDataType_Double, v, 2, nullptr, nullptr, format, flags);
 }
 
-bool ImGui::InputDouble3(const char* label, double v[3], const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputDouble3(const char* label, double v[3], const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     return InputScalarN(label, ImGuiDataType_Double, v, 3, nullptr, nullptr, format, flags);
 }
 
-bool ImGui::InputDouble4(const char* label, double v[4], const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputDouble4(const char* label, double v[4], const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     return InputScalarN(label, ImGuiDataType_Double, v, 4, nullptr, nullptr, format, flags);
 }
 
 // ###########################################################################################################
 
-bool ImGui::InputFloatL(const char* label, float* v, float v_min, float v_max, float step, float step_fast, const char* format, ImGuiInputTextFlags flags)
+bool InputFloatL(const char* label, float* v, float v_min, float v_max, float step, float step_fast, const char* format, ImGuiInputTextFlags flags)
 {
     if (InputFloat(label, v, step, step_fast, format, flags))
     {
@@ -75,7 +166,7 @@ bool ImGui::InputFloatL(const char* label, float* v, float v_min, float v_max, f
     return false;
 }
 
-bool ImGui::InputFloat2L(const char* label, float v[2], float v_min, float v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputFloat2L(const char* label, float v[2], float v_min, float v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     if (InputFloat2(label, v, format, flags))
     {
@@ -89,7 +180,7 @@ bool ImGui::InputFloat2L(const char* label, float v[2], float v_min, float v_max
     return false;
 }
 
-bool ImGui::InputFloat3L(const char* label, float v[3], float v_min, float v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputFloat3L(const char* label, float v[3], float v_min, float v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     if (InputFloat3(label, v, format, flags))
     {
@@ -103,7 +194,7 @@ bool ImGui::InputFloat3L(const char* label, float v[3], float v_min, float v_max
     return false;
 }
 
-bool ImGui::InputFloat4L(const char* label, float v[4], float v_min, float v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputFloat4L(const char* label, float v[4], float v_min, float v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     if (InputFloat4(label, v, format, flags))
     {
@@ -117,7 +208,7 @@ bool ImGui::InputFloat4L(const char* label, float v[4], float v_min, float v_max
     return false;
 }
 
-bool ImGui::InputIntL(const char* label, int* v, int v_min, int v_max, int step, int step_fast, ImGuiInputTextFlags flags)
+bool InputIntL(const char* label, int* v, int v_min, int v_max, int step, int step_fast, ImGuiInputTextFlags flags)
 {
     if (InputInt(label, v, step, step_fast, flags))
     {
@@ -127,7 +218,7 @@ bool ImGui::InputIntL(const char* label, int* v, int v_min, int v_max, int step,
     return false;
 }
 
-bool ImGui::InputInt2L(const char* label, int v[2], int v_min, int v_max, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputInt2L(const char* label, int v[2], int v_min, int v_max, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     if (InputInt2(label, v, flags))
     {
@@ -141,7 +232,7 @@ bool ImGui::InputInt2L(const char* label, int v[2], int v_min, int v_max, ImGuiI
     return false;
 }
 
-bool ImGui::InputInt3L(const char* label, int v[3], int v_min, int v_max, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputInt3L(const char* label, int v[3], int v_min, int v_max, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     if (InputInt2(label, v, flags))
     {
@@ -155,7 +246,7 @@ bool ImGui::InputInt3L(const char* label, int v[3], int v_min, int v_max, ImGuiI
     return false;
 }
 
-bool ImGui::InputInt4L(const char* label, int v[4], int v_min, int v_max, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputInt4L(const char* label, int v[4], int v_min, int v_max, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     if (InputInt2(label, v, flags))
     {
@@ -169,7 +260,7 @@ bool ImGui::InputInt4L(const char* label, int v[4], int v_min, int v_max, ImGuiI
     return false;
 }
 
-bool ImGui::InputDoubleL(const char* label, double* v, double v_min, double v_max, double step, double step_fast, const char* format, ImGuiInputTextFlags flags)
+bool InputDoubleL(const char* label, double* v, double v_min, double v_max, double step, double step_fast, const char* format, ImGuiInputTextFlags flags)
 {
     if (InputDouble(label, v, step, step_fast, format, flags))
     {
@@ -179,7 +270,7 @@ bool ImGui::InputDoubleL(const char* label, double* v, double v_min, double v_ma
     return false;
 }
 
-bool ImGui::InputDouble2L(const char* label, double v[2], double v_min, double v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputDouble2L(const char* label, double v[2], double v_min, double v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     if (InputDouble2(label, v, format, flags))
     {
@@ -193,7 +284,7 @@ bool ImGui::InputDouble2L(const char* label, double v[2], double v_min, double v
     return false;
 }
 
-bool ImGui::InputDouble3L(const char* label, double v[3], double v_min, double v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputDouble3L(const char* label, double v[3], double v_min, double v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     if (InputDouble3(label, v, format, flags))
     {
@@ -207,7 +298,7 @@ bool ImGui::InputDouble3L(const char* label, double v[3], double v_min, double v
     return false;
 }
 
-bool ImGui::InputDouble4L(const char* label, double v[4], double v_min, double v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
+bool InputDouble4L(const char* label, double v[4], double v_min, double v_max, const char* format, ImGuiInputTextFlags flags) // NOLINT(hicpp-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
 {
     if (InputDouble4(label, v, format, flags))
     {
@@ -220,3 +311,5 @@ bool ImGui::InputDouble4L(const char* label, double v[4], double v_min, double v
     }
     return false;
 }
+
+} // namespace ImGui
