7903a54f4d11c8bce6bb2dadbc56795f3dadefd9
Thomas Topp <thomas@topp.cc>
2022-01-12 16:13:31 +0100

    Remove Units lib, as not fit for our purpose and not useful withou linear algebra support

--- src/Nodes/DataProcessor/KalmanFilter/LooselyCoupledKF.cpp
+++ src/Nodes/DataProcessor/KalmanFilter/LooselyCoupledKF.cpp
@@ -19,18 +19,7 @@ namespace nm = NAV::NodeManager;
 #include "Navigation/Math/Math.hpp"
 #include "Navigation/Math/VanLoan.hpp"
 #include "Navigation/Gravity/Gravity.hpp"
-#include "Navigation/Units/Units.hpp"
 #include "util/Logger.hpp"
-
-namespace si = units::isq::si;
-
-using AttitudeUnit = units::angle<units::radian>; // Better would be degree, but library does not support this yet
-using VelocityUnit = si::speed<si::metre_per_second>;
-using LatitudeUnit = units::angle<units::pseudometre>;
-using LongitudeUnit = units::angle<units::pseudometre>;
-using AltitudeUnit = si::length<si::metre>;
-using AccelBiasUnit = si::acceleration<si::millistandardgravity>;
-using GyroBiasUnit = si::angular_velocity<si::milliradian_per_second>;
 
 NAV::LooselyCoupledKF::LooselyCoupledKF()
 {
