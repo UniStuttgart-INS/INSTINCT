ca342453a51856c557f8fbad4cfc9fefbe3310ae
Thomas Topp <thomas@topp.cc>
2022-03-13 13:39:09 +0100

    Config.ini test added

--- test/src/internal/ConfigManagerTests.cpp
+++ test/src/internal/ConfigManagerTests.cpp
@@ -105,4 +105,40 @@ TEST_CASE("[ConfigManager] Fetch configs (short options)", "[ConfigManager]")
     NAV::ConfigManager::deinitialize();
 }
 
+TEST_CASE("[ConfigManager] Fetch configs (config file)", "[ConfigManager]")
+{
+    NAV::ConfigManager::initialize();
+
+    std::vector<const char*> argv = { "",
+                                      "--config=BadConfig.ini",
+                                      "--config=test/data/config.ini",
+                                      "--load=flow/CmdOverride.flow",
+                                      nullptr };
+
+    NAV::flow::SetProgramRootPath(std::filesystem::current_path());
+    auto failedConfigFiles = NAV::ConfigManager::FetchConfigs(static_cast<int>(argv.size() - 1), argv.data());
+    NAV::flow::SetOutputPath();
+
+    REQUIRE(failedConfigFiles.size() == 1);
+    REQUIRE(failedConfigFiles.front() == "BadConfig.ini");
+
+    REQUIRE(ConfigManager::Get<bool>("sigterm", false) == true);
+    REQUIRE(ConfigManager::Get<size_t>("duration", 0) == 60);
+    REQUIRE(ConfigManager::Get<bool>("nogui", false) == true);
+
+    // The command line argument should override the config file options
+    REQUIRE(ConfigManager::Get<std::string>("load", "") == "flow/CmdOverride.flow");
+
+    REQUIRE(ConfigManager::Get<std::string>("output-path", "") == "output");
+    REQUIRE(NAV::flow::GetOutputPath() == std::filesystem::path{ NAV::flow::GetProgramRootPath() / "output" });
+
+    REQUIRE(ConfigManager::Get<std::string>("input-path", "") == "input");
+    REQUIRE(NAV::flow::GetInputPath() == std::filesystem::path{ NAV::flow::GetProgramRootPath() / "input" });
+
+    REQUIRE(ConfigManager::Get<std::string>("flow-path", "") == "flow");
+    REQUIRE(NAV::flow::GetFlowPath() == std::filesystem::path{ NAV::flow::GetProgramRootPath() / "flow" });
+
+    NAV::ConfigManager::deinitialize();
+}
+
 } // namespace NAV::TESTS
