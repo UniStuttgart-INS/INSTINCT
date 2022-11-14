c1a4fa68404d7f093b7411bae3569e2695b3b526
Thomas Topp <thomas@topp.cc>
2022-11-14 11:46:12 +0100

    Disallow empty InsTime for invokeCallback

--- test/src/Nodes/DataProvider/IMU/VectorNavFileTests.cpp
+++ test/src/Nodes/DataProvider/IMU/VectorNavFileTests.cpp
@@ -571,18 +571,11 @@ namespace DynamicSize
 void compareDynamicSizeObservation(const std::shared_ptr<const NAV::VectorNavBinaryOutput>& obs, size_t messageCounterData)
 {
     // ------------------------------------------------ InsTime --------------------------------------------------
-    if (static_cast<int32_t>(REFERENCE_DATA.at(messageCounterData).at(Ref_GpsCycle)) == -1)
-    {
-        REQUIRE(obs->insTime.empty());
-    }
-    else
-    {
-        REQUIRE(!obs->insTime.empty());
+    REQUIRE(!obs->insTime.empty());
 
-        REQUIRE(obs->insTime.toGPSweekTow().gpsCycle == static_cast<int32_t>(REFERENCE_DATA.at(messageCounterData).at(Ref_GpsCycle)));
-        REQUIRE(obs->insTime.toGPSweekTow().gpsWeek == static_cast<int32_t>(REFERENCE_DATA.at(messageCounterData).at(Ref_GpsWeek)));
-        REQUIRE(obs->insTime.toGPSweekTow().tow == Approx(REFERENCE_DATA.at(messageCounterData).at(Ref_GpsTow)).margin(EPSILON));
-    }
+    REQUIRE(obs->insTime.toGPSweekTow().gpsCycle == static_cast<int32_t>(REFERENCE_DATA.at(messageCounterData).at(Ref_GpsCycle)));
+    REQUIRE(obs->insTime.toGPSweekTow().gpsWeek == static_cast<int32_t>(REFERENCE_DATA.at(messageCounterData).at(Ref_GpsWeek)));
+    REQUIRE(obs->insTime.toGPSweekTow().tow == Approx(REFERENCE_DATA.at(messageCounterData).at(Ref_GpsTow)).margin(EPSILON));
 
     // ----------------------------------------------- TimeGroup -------------------------------------------------
     REQUIRE(obs->timeOutputs != nullptr);
